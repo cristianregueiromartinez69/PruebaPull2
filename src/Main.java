b1bc2a9d5fa77a938fba0f08db9f6a0cba2be0de
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2023-09-29 08:54:43 +0200

    modificacion en el sout

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Primer código subido al repositorio");
+        System.out.println("blabla");
+
+
+    }
+}
